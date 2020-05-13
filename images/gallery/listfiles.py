55b36e9a8f57172a37f679f265ef0244ee7e98f6
darthmorf <sampoirier@email.com>
2020-05-13 13:33:18 +0100

    Initial redesign

--- images/gallery/listfiles.py
+++
@@ -1,7 +0,0 @@
-from os import listdir
-from os.path import isfile, join
-mypath = "./"
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-
-for file in onlyfiles:
-    print('<a href="'+file+'" data-ngthumb="'+file+'" data-ngdesc="">'+file+'</a>')
